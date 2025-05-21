8b14778e4d217e9be4e229321833d9d7275e29bc
Arya Wahyu <100331706+aryaw05@users.noreply.github.com>
2025-05-21 11:30:30 +0700

    feat : Implement login page rendering via Inertia.js

--- resources/js/Pages/Admin/login.jsx
+++ resources/js/Pages/Admin/login.jsx
@@ -1,39 +1,68 @@
+import { router } from "@inertiajs/react";
+import { useState } from "react";
 
+const LoginSatria = (props) => {
+    const { errors } = props;
+    const [formData, setFormData] = useState();
+    console.log(errors);
+    const [error, setError] = useState([]);
 
-const LoginSatria = () => {
-  return (
-    <div className="bg-gray-200 min-h-screen flex items-center justify-center px-4">
-      <div className="rounded-3xl w-full sm:w-[90%] md:w-[70%] lg:w-[40%] xl:w-[30%] max-w-md p-6 py-10 bg-white shadow-lg">
-        <h1 className="text-2xl font-semibold text-center mb-4">Login SATRIA</h1>
-        <form className="space-y-4 px-2">
-          <div>
-            <label className="text-sm text-black">Username</label>
-            <input
-              type="text"
-              className="input w-full bg-gray-100 p-2 rounded-md"
-              placeholder="Masukkan Username"
-            />
-          </div>
-          <div>
-            <label className="text-sm text-black">Password</label>
-            <input
-              type="password"
-              className="input w-full bg-gray-100 p-2 rounded-md"
-              placeholder="Masukkan Password"
-            />
-          </div>
-          <div className="flex justify-center pt-4">
-            <button
-              type="submit"
-              className="btn bg-orange-500 w-4/5 text-white p-2 rounded-md hover:bg-orange-600 transition"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const res = router.post("/login", formData);
+        console.log(res);
+        console.log(formData);
+    };
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    return (
+        <div className="bg-gray-200 min-h-screen flex items-center justify-center px-4">
+            <div className="rounded-3xl w-full sm:w-[90%] md:w-[70%] lg:w-[40%] xl:w-[30%] max-w-md p-6 py-10 bg-white shadow-lg">
+                <h1 className="text-2xl font-semibold text-center mb-4">
+                    Login
+                </h1>
+                <form
+                    className="space-y-4 px-2"
+                    method="POST"
+                    onSubmit={handleSubmit}
+                >
+                    <div>
+                        <label className="text-sm text-black">Username</label>
+                        <input
+                            name="username"
+                            type="text"
+                            className="input w-full bg-gray-100 p-2 rounded-md"
+                            placeholder="Masukkan Username"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div>
+                        <label className="text-sm text-black">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            className="input w-full bg-gray-100 p-2 rounded-md"
+                            placeholder="Masukkan Password"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="flex justify-center pt-4">
+                        <button
+                            type="submit"
+                            className="btn bg-orange-500 w-4/5 text-white p-2 rounded-md hover:bg-orange-600 transition"
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default LoginSatria;
