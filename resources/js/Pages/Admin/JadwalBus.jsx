b2fcd95449a6f3a56eb972ed55829a6f5e40bf92
aryaw05 <aryawahyuaw77@gmail.com>
2025-06-26 10:15:53 +0700

    refactor : refactor crud jadwal bus

---
+++ resources/js/Pages/Admin/JadwalBus.jsx
@@ -0,0 +1,315 @@
+import { useEffect, useState } from "react";
+import useActionForm from "../../hooks/useActionForm";
+import { handleDelete, handleEdit, handleSubmit } from "../../utils/handleCRUD";
+
+export default function JadwalBus(props) {
+    const { buses, halte, jadwal } = props;
+
+    const { formData, handleChange, setFormData } = useActionForm();
+    const [dataJadwal, setdataJadwal] = useState({
+        indexBus: null,
+    });
+
+    useEffect(() => {
+        if (dataJadwal?.indexBus !== null) {
+            const jadwalBus = jadwal[dataJadwal.indexBus];
+            console.log("jadwal bus", jadwalBus);
+
+            setFormData({
+                id_jadwal: jadwalBus.id_jadwal || "",
+                id_bus: jadwalBus.id_bus || "",
+                id_halte: jadwalBus.id_halte || "",
+                waktu_berangkat: jadwalBus.waktu_berangkat || "",
+                waktu_tiba: jadwalBus.waktu_tiba || "",
+            });
+        }
+    }, [dataJadwal]);
+
+    function editDataJadwal(modalId, index) {
+        const modal = document.getElementById(modalId);
+        if (modal) modal.showModal();
+        setdataJadwal({
+            indexBus: index,
+        });
+    }
+
+    function addDataJadwal(modalId) {
+        const modal = document.getElementById(modalId);
+        if (modal) modal.showModal();
+    }
+    return (
+        <>
+            <div className="lg:w-1/2 w-full">
+                <button
+                    className="btn bg-orange-400 mb-3 rounded-lg w-1/2 lg:w-auto"
+                    onClick={() => addDataJadwal("my_modal_1")}
+                >
+                    + Tambah Jadwal
+                </button>
+                <div className="overflow-auto">
+                    <table className="table border border-collapse text-center w-full min-w-[500px]">
+                        <thead className="bg-orange-400">
+                            <tr>
+                                <th className="border">Nama Bus</th>
+                                <th className="border">Nama Halte</th>
+                                <th className="border">Waktu Berangkat</th>
+                                <th className="border">Waktu Tiba</th>
+                                <th className="border">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {jadwal?.map((e, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td className="border">
+                                            {e.bus.nomor_bus}
+                                        </td>
+                                        <td className="border">
+                                            {e.halte.nama_halte}
+                                        </td>
+                                        <td className="border">
+                                            {" "}
+                                            {e.waktu_berangkat}
+                                        </td>
+                                        <td className="border">
+                                            {e.waktu_tiba}
+                                        </td>
+                                        <td className="border">
+                                            <div className="justify-center flex flex-col gap-2">
+                                                <button
+                                                    className="btn btn-error"
+                                                    onClick={() =>
+                                                        handleDelete(
+                                                            e.id_jadwal,
+                                                            "admin/dashboard/jadwal/delete"
+                                                        )
+                                                    }
+                                                >
+                                                    Delete
+                                                </button>
+                                                <button
+                                                    className="btn btn-info"
+                                                    onClick={() =>
+                                                        editDataJadwal(
+                                                            "my_modal_4",
+                                                            index
+                                                        )
+                                                    }
+                                                >
+                                                    Edit
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            {/* Modal Tambah Jadwal */}
+            <dialog id="my_modal_1" className="modal">
+                <div className="modal-box rounded-3xl w-[95%] max-w-md">
+                    <form method="dialog">
+                        <button
+                            className="btn btn-lg btn-circle absolute border-transparent right-4 top-4 bg-transparent hover:bg-transparent !hover:text-black"
+                            onClick={() =>
+                                setdataJadwal({
+                                    indexBus: null,
+                                })
+                            }
+                        >
+                            ✕
+                        </button>
+                    </form>
+
+                    <h1 className="text-2xl font-semibold text-center pt-5 mb-4">
+                        Tambah Jadwal Bus
+                    </h1>
+                    <form
+                        className="space-y-4 px-2 py-4"
+                        onSubmit={(e) => {
+                            handleSubmit(
+                                e,
+                                "/admin/dashboard/jadwal/add",
+                                formData
+                            );
+                        }}
+                    >
+                        <div>
+                            <label className="text-sm text-black">
+                                Nama Bus
+                            </label>
+                            <select
+                                className="select w-full bg-gray-100"
+                                onChange={handleChange}
+                                name="id_bus"
+                            >
+                                <option disabled>Pilih Nama Bus</option>
+                                {buses.map((buss) => (
+                                    <option
+                                        key={buss.id_bus}
+                                        value={buss.id_bus}
+                                    >
+                                        {buss.nomor_bus}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div>
+                            <label className="text-sm text-black">
+                                Nama Halte
+                            </label>
+                            <select
+                                className="select select-bordered w-full"
+                                onChange={handleChange}
+                                name="id_halte"
+                            >
+                                {halte.map((h) => (
+                                    <option key={h.id_halte} value={h.id_halte}>
+                                        {h.nama_halte}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div>
+                            <label className="text-sm text-black">
+                                Waktu Berangkat
+                            </label>
+                            <input
+                                onChange={handleChange}
+                                required
+                                name="waktu_berangkat"
+                                type="time"
+                                className="input w-full bg-gray-100"
+                            />
+                        </div>
+                        <div>
+                            <label className="text-sm text-black">
+                                Waktu Tiba
+                            </label>
+                            <input
+                                onChange={handleChange}
+                                required
+                                name="waktu_tiba"
+                                type="time"
+                                className="input w-full bg-gray-100"
+                            />
+                        </div>
+                        <div className="flex justify-center pt-4">
+                            <button className="btn bg-orange-500 w-4/5 text-white">
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </dialog>
+
+            {/* Modal edit Data Jadwal */}
+            <dialog id="my_modal_4" className="modal">
+                <div className="modal-box rounded-3xl w-[95%] max-w-md">
+                    <form method="dialog">
+                        <button
+                            className="btn btn-lg btn-circle absolute border-transparent right-4 top-4 bg-transparent hover:bg-transparent !hover:text-black"
+                            onClick={() =>
+                                setdataJadwal({
+                                    idBus: null,
+                                    indexBus: null,
+                                    component: null,
+                                })
+                            }
+                        >
+                            ✕
+                        </button>
+                    </form>
+
+                    <h1 className="text-2xl font-semibold text-center pt-5 mb-4">
+                        Edit Jadwal Bus
+                    </h1>
+                    <form
+                        className="space-y-4 px-2 py-4"
+                        onSubmit={(e) => {
+                            handleEdit(
+                                e,
+                                "admin/dashboard/jadwal/edit",
+                                formData?.id_jadwal,
+                                formData
+                            );
+                        }}
+                    >
+                        <div>
+                            <label className="text-sm text-black">
+                                Nama Bus
+                            </label>
+                            <select
+                                className="select w-full bg-gray-100"
+                                onChange={handleChange}
+                                name="id_bus"
+                            >
+                                <option value={formData?.id_bus}>
+                                    {
+                                        jadwal[dataJadwal?.indexBus]?.bus
+                                            ?.nomor_bus
+                                    }
+                                </option>
+                            </select>
+                        </div>
+                        <div>
+                            <label className="text-sm text-black">
+                                Nama Halte
+                            </label>
+                            <select
+                                className="select select-bordered w-full"
+                                onChange={handleChange}
+                                name="id_halte"
+                            >
+                                <option value={formData?.id_halte}>
+                                    {
+                                        halte?.find(
+                                            (h) =>
+                                                h.id_halte ===
+                                                formData?.id_halte
+                                        )?.nama_halte
+                                    }
+                                </option>
+                            </select>
+                        </div>
+                        <div>
+                            <label className="text-sm text-black">
+                                Waktu Berangkat
+                            </label>
+                            <input
+                                onChange={handleChange}
+                                required
+                                value={formData.waktu_berangkat || ""}
+                                name="waktu_berangkat"
+                                type="time"
+                                step="1"
+                                className="input w-full bg-gray-100"
+                            />
+                        </div>
+                        <div>
+                            <label className="text-sm text-black">
+                                Waktu Tiba
+                            </label>
+                            <input
+                                onChange={handleChange}
+                                required
+                                value={formData.waktu_tiba || ""}
+                                name="waktu_tiba"
+                                type="time"
+                                step="1"
+                                className="input w-full bg-gray-100"
+                            />
+                        </div>
+                        <div className="flex justify-center pt-4">
+                            <button className="btn bg-orange-500 w-4/5 text-white ">
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </dialog>
+        </>
+    );
+}
--- resources/js/utils/handleCRUD.jsx
+++ resources/js/utils/handleCRUD.jsx
@@ -4,39 +4,37 @@ function handleSubmit(e, route, formData) {
     router.post(route, formData, {
         onError: (errors) => {
             console.log("Error:", errors);
-        }
+        },
     });
-    
 }
 
 function handleDelete(id, route) {
-     const res = router.delete(`/${route}/${id}`);
-     console.log(res);
+    const res = router.delete(`/${route}/${id}`);
 }
 
-function handleEdit (e ,route, id, formData) {
+function handleEdit(e, route, id, formData) {
     e.preventDefault();
-    router.put(`/${route}/${id}`, formData , 
-{
-
-    onError: (errors) => {
-        console.log("Error:", errors);
-    }
-}
- );
+    router.put(`/${route}/${id}`, formData, {
+        onError: (errors) => {
+            console.log("Error:", errors);
+        },
+    });
 }
 
-function handleEditNotForm (route, id, formData) {
- router.put(`/${route}/${id}`, formData , 
-{
-    onError: (errors) => {
-        console.log("Error:", errors);
-    }
-}
- );
+function handleEditNotForm(route, id, formData) {
+    router.put(`/${route}/${id}`, formData, {
+        onError: (errors) => {
+            console.log("Error:", errors);
+        },
+    });
 }
 function handleLogout(route) {
     const res = router.post(`/${route}`);
 }
-export { handleSubmit, handleDelete, handleLogout , handleEdit  , handleEditNotForm};
-
+export {
+    handleSubmit,
+    handleDelete,
+    handleLogout,
+    handleEdit,
+    handleEditNotForm,
+};
