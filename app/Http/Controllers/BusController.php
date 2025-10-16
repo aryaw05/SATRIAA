368c45d9e7f85c6fe2feaf60e7c918967c86eaed
bilalrafael <bilalrafaelyunanta@gmail.com>
2025-10-16 20:11:29 +0700

    update logout button bus

--- app/Http/Controllers/BusController.php
+++ app/Http/Controllers/BusController.php
@@ -26,7 +26,7 @@ class BusController extends Controller
             'kapasitas_tempat_duduk' => 'integer',
             'status' => 'nullable|string|max:255',
             'kondisi' => 'string|max:255',
-            'password' => 'required|string|min:3',
+            'password' => 'required|string|min:6',
         ]);
         $data = $request->all();
 
--- resources/js/Pages/Kernet/gps.jsx
+++ resources/js/Pages/Kernet/gps.jsx
@@ -20,6 +20,12 @@ const GpsSatria = (props) => {
     console.log("duduk", kepadatan);
 
     const toggleGps = () => setIsActive((prev) => !prev);
+    const handleLogout = () => {
+        // konfirmasi sebelum logout
+        if (confirm("Yakin ingin logout dari sistem?")) {
+            router.post("/logoutBus"); // sesuai route logout di Laravel
+        }
+    };
     const updateLocation = () => {
         if (!navigator.geolocation) {
             console.warn("Geolocation tidak didukung");
@@ -130,9 +136,13 @@ const GpsSatria = (props) => {
             {/* Konten */}
             <div className="bg-gray-100 min-h-screen mx-5">
                 <div className="bg-white rounded-3xl mt-10 mx-5 px-6 py-6 w-full max-w-md mx-auto">
-                    <h1 className="font-bold text-2xl mb-0">{bus.nomor_bus}</h1>
-
-                    <h2 className="text-gray-400 text-xl mb-3">ON/OFF GPS</h2>
+                    <div className="flex">
+                        <div className="">
+                            <h1 className="font-bold text-2xl mb-0">{bus.nomor_bus}</h1>
+                            <h2 className="text-gray-400 text-xl mb-3">ON/OFF GPS</h2>
+                        </div>
+                        <button onClick={handleLogout} className="bg-red-500 rounded-md px-6 h-12 shadow-md text-white font-bold hover:bg-red-400 cursor-pointer ml-auto">Logout</button>
+                    </div>
 
                     {/* ON/OFF Toggle */}
                     <div className="flex items-center justify-between mb-1">
