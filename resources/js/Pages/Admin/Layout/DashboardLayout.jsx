d5044b9d77dc806fb8a45865802d14caae54cab2
aryaw05 <aryawahyuaw77@gmail.com>
2025-10-20 17:48:16 +0700

    refactor(admin): simplify dashboard layout by using shared components

--- resources/js/Pages/Admin/Layout/DashboardLayout.jsx
+++ resources/js/Pages/Admin/Layout/DashboardLayout.jsx
@@ -1,10 +1,9 @@
 import { Head } from "@inertiajs/react";
-import Navbar from "../Navbar";
-import JadwalBus from "../JadwalBus";
-import InputBus from "../InputBus";
+import Sidebar from "../../../components/Sidebar";
+import Navbar from "../../../components/navbar/Navbar";
 
 const DashboardLayout = (props) => {
-    const { buses, jadwal, halte } = props;
+    const { children } = props;
 
     return (
         <>
@@ -12,25 +11,10 @@ const DashboardLayout = (props) => {
                 <title>Dashboard</title>
             </Head>
 
-            <div className="min-h-screen">
-                {/* Navbar */}
+            <Sidebar>
                 <Navbar />
-
-                {/* Konten */}
-                <div className="px-4 py-6 pt-23">
-                    <div className="flex flex-col lg:flex-row gap-6">
-                        {/* Bagian kiri - Jadwal */}
-                        <JadwalBus
-                            jadwal={jadwal}
-                            buses={buses}
-                            halte={halte}
-                        />
-
-                        {/* Bagian kanan - Data Bus */}
-                        <InputBus buses={buses} jadwal={jadwal} halte={halte} />
-                    </div>
-                </div>
-            </div>
+                {children}
+            </Sidebar>
         </>
     );
 };
