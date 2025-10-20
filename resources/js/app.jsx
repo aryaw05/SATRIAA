dae76b03ec81095b99d8af83fc135e542eb88005
aryaw05 <aryawahyuaw77@gmail.com>
2025-10-20 17:47:13 +0700

    feat(styles): update font family and add fontawesome icons

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,18 +1,22 @@
 import { createInertiaApp } from "@inertiajs/react";
 import { createRoot } from "react-dom/client";
 import { library } from "@fortawesome/fontawesome-svg-core";
-
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-
 import {
     faLocationCrosshairs,
     faBus,
     faWheelchair,
     faEllipsis,
     faCircleUser,
+    faUserTie,
+    faCalendarDays,
+    faHouse,
+    faRightFromBracket,
+    faUsers,
 } from "@fortawesome/free-solid-svg-icons";
-import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import "./echo";
+import "@fontsource/poppins/100.css";
+import "@fontsource/poppins/400.css";
 createInertiaApp({
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
@@ -27,7 +31,11 @@ createInertiaApp({
             faWheelchair,
             faEllipsis,
             faCircleUser,
-            faRightFromBracket
+            faRightFromBracket,
+            faUserTie,
+            faCalendarDays,
+            faHouse,
+            faUsers
         );
     },
 });
